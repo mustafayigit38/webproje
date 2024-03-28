1f59855c90582b3afe228041f8dd81ac1d38fee4
mustafayigit38 <csmustafa887@gmail.com>
2024-03-28 11:59:18 +0300

    fihris dosayası eklendi

---
+++ Fihris/Controllers/PersonController.cs
@@ -0,0 +1,34 @@
+﻿using Fihris.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fihris.Controllers
+{
+    public class PersonController : Controller
+    {
+        public FihrisContext Context { get; set; }
+        public PersonController()
+        {
+            Context = new FihrisContext();
+        }
+        public IActionResult Index()
+        {
+            var liste = Context.People.ToList();
+            return View(liste);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create([FromForm]Person p) 
+        {
+            if(ModelState.IsValid)
+            {
+                Context.People.Add(p);
+                Context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(p);
+        }
+    }
+}
